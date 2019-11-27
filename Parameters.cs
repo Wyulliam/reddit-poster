bda0a804e363e50537b3736e865329feb3c9e956
wyulliam <william.da.silva@verizonconnect.com>
2019-11-27 17:05:03 +0000

    Making it better

--- Parameters.cs
+++ Parameters.cs
@@ -5,7 +5,7 @@
         public static string Subredit { get; set; }
         public static int PostCount { get; set; }
         public static string FolderPath { get; set; }
-        public static string PostTitle => $"The entire Shrek movie but with a 3 seconds timespan - PART {PostCount}";
+        public static string PostTitle => $"The entire Bee Movie but with a 3 seconds timespan - PART {PostCount}";
         public static string NotPostedFolderPath => $"{FolderPath}\\not posted";
     }
 }
